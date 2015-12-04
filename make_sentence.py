ac29f93e25ab281f73253f16762833bbd05a7234
fernavid <fernando@saucelabs.com>
2015-12-03 16:56:06 -0800

    tweaked the next word logic a bit

--- make_sentence.py
+++ make_sentence.py
@@ -1,33 +1,53 @@
-import sys
+import sys, math, random
 
 import model
+from model import WordEntry
 
 from sqlalchemy.sql.expression import func
+from sqlalchemy import Column, Integer, MetaData, String, Table, desc
 
-
+#for each possible next word sampled from the crowd, generate PERSONALITY_WEIGHT entries from the actual user
+PERSONALITY_WEIGHT = 15
+SENTENCE_WORD_MINIMUM = 2
 SENTENCE_WORD_LIMIT = 100
-
 
 session = model.get_session()
 
+def get_next_word(username, word):
+    words = session.query(WordEntry)\
+            .filter(WordEntry.word_prev == word)\
+            .order_by(desc(WordEntry.count)).limit(500)
+
+    candidates = []
+    for w in words:
+        if w.user == username:
+            for i in range(PERSONALITY_WEIGHT):
+                candidates.append(w.word_next)
+        candidates.append(w.word_next)
+
+    result = random.choice(candidates)
+    return result
 
 def make_sentence(username):
-
-    # Try to find the user
-    user = session.query(model.User).filter(model.User.name==username).first()
-    if not user:
-        raise Exception('Username {} not found'.format(username))
-
     sentence = ''
-    # Load up an initial random word
-    word = session.query(model.WordEntry)\
-        .filter(model.WordEntry.user == user.id, model.WordEntry.word_prev == '')\
-        .order_by(func.rand()).first()
-    sentence += word.word_next
-    for i in xrange(SENTENCE_WORD_LIMIT):
-        word = word.next(session)
-        if word.word_next:
-            sentence += ' ' + word.word_next
-        else:
-            break
+    while len(sentence.split(' ')) < SENTENCE_WORD_MINIMUM:
+        # Try to find the user
+        user = session.query(model.User).filter(model.User.name==username).first()
+        if not user:
+            raise Exception('Username {} not found'.format(username))
+
+        sentence = ''
+        # Load up an initial random word
+        word = session.query(model.WordEntry)\
+            .filter(model.WordEntry.user == user.id, model.WordEntry.word_prev == '')\
+            .order_by(func.rand()).first()
+
+        next_word = word.word_next
+        sentence += next_word
+        for i in xrange(SENTENCE_WORD_LIMIT):
+            next_word = get_next_word(username, next_word)
+            if next_word:
+                sentence += ' ' + next_word
+            else:
+                break
     return sentence

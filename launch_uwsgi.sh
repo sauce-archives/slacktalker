277e1f5002e7f7802c4a6c74a06eca9e5ac10f86
Dan Ellis <dan.ellis@saucelabs.com>
2015-12-05 15:50:19 -0800

    Append log.

--- launch_uwsgi.sh
+++ launch_uwsgi.sh
@@ -1,3 +1,3 @@
 source ~/venv/bin/activate
 pkill uwsgi
-uwsgi -s /tmp/uwsgi.sock --module webapp --callable app --chmod-socket=666 > /tmp/webapp.log 2>&1 &
+uwsgi -s /tmp/uwsgi.sock --module webapp --callable app --chmod-socket=666 >> /tmp/webapp.log 2>&1 &
